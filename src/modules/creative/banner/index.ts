1bc4cfb10ef5937a5fd741902fc1ef5a88326c91
Kai Miyamoto <kaihogehoge@gmail.com>
2025-01-09 06:28:33 +0900

    feat: メジャーなバナーサイズを追加

--- src/modules/creative/banner/index.ts
+++ src/modules/creative/banner/index.ts
@@ -9,4 +9,52 @@ export class BannerModule extends Module {
       )
     ).markup;
   }
+
+  public s336x280(): string {
+    return this.helper.selectRandomArrayItem<BannerDefinition>(
+      this.definitions.creative.banner.filter(
+        (banner) => banner.size === "336x280"
+      )
+    ).markup;
+  }
+
+  public s728x90(): string {
+    return this.helper.selectRandomArrayItem<BannerDefinition>(
+      this.definitions.creative.banner.filter(
+        (banner) => banner.size === "728x90"
+      )
+    ).markup;
+  }
+
+  public s160x600(): string {
+    return this.helper.selectRandomArrayItem<BannerDefinition>(
+      this.definitions.creative.banner.filter(
+        (banner) => banner.size === "160x600"
+      )
+    ).markup;
+  }
+
+  public s300x600(): string {
+    return this.helper.selectRandomArrayItem<BannerDefinition>(
+      this.definitions.creative.banner.filter(
+        (banner) => banner.size === "300x600"
+      )
+    ).markup;
+  }
+
+  public s320x50(): string {
+    return this.helper.selectRandomArrayItem<BannerDefinition>(
+      this.definitions.creative.banner.filter(
+        (banner) => banner.size === "320x50"
+      )
+    ).markup;
+  }
+
+  public s320x100(): string {
+    return this.helper.selectRandomArrayItem<BannerDefinition>(
+      this.definitions.creative.banner.filter(
+        (banner) => banner.size === "320x100"
+      )
+    ).markup;
+  }
 }
--- test/units/modules/creative/BannerModule.test.ts
+++ test/units/modules/creative/BannerModule.test.ts
@@ -3,18 +3,84 @@ import { Helper } from "@/helper";
 import { BannerModule } from "@/modules/creative/banner";
 
 describe("Banner Module Behavior", () => {
-  it("300x250のバナークリエイティブを生成する", () => {
-    const helper = new Helper();
-    vi.spyOn(helper, 'selectRandomArrayItem').mockReturnValue({
-        markup: '<div>banner ad 300x250</div>'
-    });
-    const sut = new BannerModule({
+  let helper: Helper;
+  let sut: BannerModule;
+
+  beforeEach(() => {
+    helper = new Helper();
+    sut = new BannerModule({
       helper: helper,
       definitions: data,
+    });
+  });
+
+  it("300x250のバナークリエイティブを生成する", () => {
+    vi.spyOn(helper, 'selectRandomArrayItem').mockReturnValue({
+      markup: '<div>banner ad 300x250</div>'
     });
 
     const result = sut.s300x250();
 
     expect(result).toBe('<div>banner ad 300x250</div>');
+  });
+
+  it("336x280のバナークリエイティブを生成する", () => {
+    vi.spyOn(helper, 'selectRandomArrayItem').mockReturnValue({
+      markup: '<div>banner ad 336x280</div>'
+    });
+
+    const result = sut.s336x280();
+
+    expect(result).toBe('<div>banner ad 336x280</div>');
+  });
+
+  it("728x90のバナークリエイティブを生成する", () => {
+    vi.spyOn(helper, 'selectRandomArrayItem').mockReturnValue({
+      markup: '<div>banner ad 728x90</div>'
+    });
+
+    const result = sut.s728x90();
+
+    expect(result).toBe('<div>banner ad 728x90</div>');
+  });
+
+  it("160x600のバナークリエイティブを生成する", () => {
+    vi.spyOn(helper, 'selectRandomArrayItem').mockReturnValue({
+      markup: '<div>banner ad 160x600</div>'
+    });
+
+    const result = sut.s160x600();
+
+    expect(result).toBe('<div>banner ad 160x600</div>');
+  });
+
+  it("300x600のバナークリエイティブを生成する", () => {
+    vi.spyOn(helper, 'selectRandomArrayItem').mockReturnValue({
+      markup: '<div>banner ad 300x600</div>'
+    });
+
+    const result = sut.s300x600();
+
+    expect(result).toBe('<div>banner ad 300x600</div>');
+  });
+
+  it("320x50のバナークリエイティブを生成する", () => {
+    vi.spyOn(helper, 'selectRandomArrayItem').mockReturnValue({
+      markup: '<div>banner ad 320x50</div>'
+    });
+
+    const result = sut.s320x50();
+
+    expect(result).toBe('<div>banner ad 320x50</div>');
+  });
+
+  it("320x100のバナークリエイティブを生成する", () => {
+    vi.spyOn(helper, 'selectRandomArrayItem').mockReturnValue({
+      markup: '<div>banner ad 320x100</div>'
+    });
+
+    const result = sut.s320x100();
+
+    expect(result).toBe('<div>banner ad 320x100</div>');
   });
 });
