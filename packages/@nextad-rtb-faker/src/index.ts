93afa5d6f411f6f915ee72f38d5b5be62dbd36c3
Kai Miyamoto <kaihogehoge@gmail.com>
2025-02-24 08:27:18 +0900

    feat(rtb-faker): Initial package structure and add OpenRTB v26 endpoint with validation

--- packages/@nextad-rtb-faker/src/index.ts
+++ packages/@nextad-rtb-faker/src/index.ts
@@ -1,9 +1,63 @@
-import { Hono } from 'hono'
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { bidRequestSchema } from "@nextad/openrtb/schema/v26";
+import { faker } from "@nextad/faker";
+import { AudioVideoCreativeSubtype } from "iab-adcom";
+import { Asset } from "iab-native";
+import { NoBidReasonCode } from "iab-openrtb/v26";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/openrtb/v26", zValidator("json", bidRequestSchema), (c) => {
+  const validatedBidRequest = c.req.valid("json");
 
-export default app
+  let bidResponse = faker.openrtb.v26.bidResponse;
+
+  bidResponse.beginSeatBid("nextadjs");
+  for (let imp of validatedBidRequest.imp) {
+    if (imp.banner) {
+      const defaultFormat = { w: 300, h: 250 };
+      const format = imp.banner?.format?.[0] || defaultFormat;
+
+      const width = imp.banner?.w || format.w || 300;
+      const height = imp.banner?.h || format.h || 250;
+
+      bidResponse.addBannerBid({
+        impid: imp.id,
+        adm: faker.creative.display.size(width, height),
+        attr: [12],
+      });
+    } else if (imp.video) {
+      bidResponse.addVideoBid({
+        impid: imp.id,
+        adm: faker.creative.vast.v42(), // TODO: support to version switch
+        protocol: AudioVideoCreativeSubtype.VAST_4_2,
+        // TODO: support to api framework
+      });
+    } else if (imp.native) {
+      bidResponse.addNativeBid(
+        faker.creative.native.request(
+          JSON.parse(imp.native.request).assets as unknown as Asset[]
+        ),
+        {
+          impid: imp.id,
+        }
+      );
+    }
+  }
+
+  faker.reset();
+
+  return c.json(bidResponse.make());
+});
+
+// nbr point
+app.post("openrtb/v26/nbr/:nbr", (c) => {
+  const nbr = (Number(c.req.query("nbr")) || 0) as unknown as NoBidReasonCode;
+  let bidResponse = faker.openrtb.v26.bidResponse;
+  return c.json(bidResponse.nbr(nbr));
+});
+
+// probility point
+
+export default app;
